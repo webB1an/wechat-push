71d4fef9df97ac901f182f4af0c00918d59b18e5
webB1an <webB1anyaoyao@gmail.com>
2024-06-25 14:17:50 +0800

    测试 appid

--- cron/index.ts
+++ cron/index.ts
@@ -3,7 +3,7 @@ import { CronJob } from 'cron'
 import { wechat } from '../wechat'
 
 export const job = new CronJob(
-  '0 30 7 * * *', // cronTime
+  '0 30 14 * * *', // cronTime
   function() {
     console.log('执行了')
     console.log(process.env.APP_ID)
