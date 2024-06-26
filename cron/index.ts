292551279676f8da1cdf83aba89eca8c352c859e
webB1an <webB1anyaoyao@gmail.com>
2024-06-26 18:00:58 +0800

    测试定时任务

--- cron/index.ts
+++ cron/index.ts
@@ -3,7 +3,7 @@ import { CronJob } from 'cron'
 import { wechat } from '../wechat'
 
 export const job = new CronJob(
-  '0 30 7 * * *', // cronTime
+  '0 30 9 * * *', // cronTime
   function() {
     console.log('执行了')
     console.log(process.env.APP_ID)
